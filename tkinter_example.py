50246cf1910c4eca65bb189cfdb0e8ef1a832e70
Joseph Micallef <micallefje@gmail.com>
2025-06-12 18:53:01 +0100

    add button and text box

--- tkinter_example.py
+++ tkinter_example.py
@@ -3,4 +3,22 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Test App")
 
+def add_to_list():
+    text = entry.get()
+    if text:
+        text_list.insert(tk.END, text)
+        entry.delete(0, tk.END)
+
+frame = tk.Frame(root)
+frame.grid(row=0, column=0)
+
+entry = tk.Entry(frame)
+entry.grid(row=0, column=0)
+
+entry_btn = tk.Button(frame, text="Add", command=add_to_list)
+entry_btn.grid(row=0, column=1)
+
+text_list = tk.Listbox(frame)
+text_list.grid(row=1, column=0)
+
 root.mainloop()
