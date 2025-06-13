4bf8537196c90ae41acf47d1bd3df0ee5430b0c5
Joseph Micallef <micallefje@gmail.com>
2025-06-13 11:46:47 +0100

    add binding of return key

--- tkinter_example.py
+++ tkinter_example.py
@@ -15,10 +15,12 @@ frame.grid(row=0, column=0)
 entry = tk.Entry(frame)
 entry.grid(row=0, column=0)
 
+entry.bind("<Return>", lambda event: add_to_list())
+
 entry_btn = tk.Button(frame, text="Add", command=add_to_list)
 entry_btn.grid(row=0, column=1)
 
 text_list = tk.Listbox(frame)
-text_list.grid(row=1, column=0)
+text_list.grid(row=1, column=0, columnspan=2, sticky="ew")
 
 root.mainloop()
