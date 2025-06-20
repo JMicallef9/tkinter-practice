e0cca090568d67736c53170c3c33f03518e322c6
Joseph Micallef <micallefje@gmail.com>
2025-06-20 15:12:36 +0100

    adjust resizing of widgets

--- tkinter_example.py
+++ tkinter_example.py
@@ -9,11 +9,18 @@ def add_to_list():
         text_list.insert(tk.END, text)
         entry.delete(0, tk.END)
 
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
 frame = tk.Frame(root)
-frame.grid(row=0, column=0)
+frame.grid(row=0, column=0, sticky="nsew")
+
+frame.columnconfigure(0, weight=1)
+frame.rowconfigure(1, weight=1)
+
 
 entry = tk.Entry(frame)
-entry.grid(row=0, column=0)
+entry.grid(row=0, column=0, sticky="ew")
 
 entry.bind("<Return>", lambda event: add_to_list())
 
@@ -21,6 +28,6 @@ entry_btn = tk.Button(frame, text="Add", command=add_to_list)
 entry_btn.grid(row=0, column=1)
 
 text_list = tk.Listbox(frame)
-text_list.grid(row=1, column=0, columnspan=2, sticky="ew")
+text_list.grid(row=1, column=0, columnspan=2, sticky="nsew")
 
 root.mainloop()
