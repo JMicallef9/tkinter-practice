8886b368484fb17d3f2c50cf134e1a621012dc68
Joseph Micallef <micallefje@gmail.com>
2025-06-21 11:04:58 +0100

    add second frame

--- tkinter_example.py
+++ tkinter_example.py
@@ -18,7 +18,6 @@ frame.grid(row=0, column=0, sticky="nsew")
 frame.columnconfigure(0, weight=1)
 frame.rowconfigure(1, weight=1)
 
-
 entry = tk.Entry(frame)
 entry.grid(row=0, column=0, sticky="ew")
 
@@ -28,6 +27,23 @@ entry_btn = tk.Button(frame, text="Add", command=add_to_list)
 entry_btn.grid(row=0, column=1)
 
 text_list = tk.Listbox(frame)
+text_list.grid(row=1, column=0, columnspan=2, sticky="nsew")
+
+frame2 = tk.Frame(root)
+frame2.grid(row=0, column=1, sticky="nsew")
+
+frame2.columnconfigure(0, weight=1)
+frame2.rowconfigure(1, weight=1)
+
+entry = tk.Entry(frame2)
+entry.grid(row=0, column=0, sticky="ew")
+
+entry.bind("<Return>", lambda event: add_to_list())
+
+entry_btn = tk.Button(frame2, text="Add", command=add_to_list)
+entry_btn.grid(row=0, column=1)
+
+text_list = tk.Listbox(frame2)
 text_list.grid(row=1, column=0, columnspan=2, sticky="nsew")
 
 root.mainloop()
