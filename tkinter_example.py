933d697dfa33f4e7264db258e023218aa3308f14
Joseph Micallef <micallefje@gmail.com>
2025-06-23 12:03:38 +0100

    fix button behaviours with extra lambdas

--- tkinter_example.py
+++ tkinter_example.py
@@ -28,9 +28,8 @@ text_list.grid(row=1, column=0, columnspan=2, sticky="nsew")
 
 entry.bind("<Return>", lambda event: add_to_list(entry, text_list))
 
-entry_btn = ttk.Button(frame, text="Add", command=add_to_list(entry, text_list))
+entry_btn = ttk.Button(frame, text="Add", command=lambda: add_to_list(entry, text_list))
 entry_btn.grid(row=0, column=1)
-
 
 frame2 = tk.Frame(root)
 frame2.grid(row=0, column=1, sticky="nsew", padx=5, pady=5)
@@ -46,7 +45,7 @@ text_list2.grid(row=1, column=0, columnspan=2, sticky="nsew")
 
 entry2.bind("<Return>", lambda event: add_to_list(entry2, text_list2))
 
-entry_btn2 = tk.Button(frame2, text="Add", command=add_to_list(entry2, text_list2))
+entry_btn2 = tk.Button(frame2, text="Add", command=lambda: add_to_list(entry2, text_list2))
 entry_btn2.grid(row=0, column=1)
 
 root.mainloop()
