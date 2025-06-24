439b796b6660d9f33882db65bea0ab14217d53f4
Joseph Micallef <micallefje@gmail.com>
2025-06-24 12:37:07 +0100

    add main function

--- tkinter_example.py
+++ tkinter_example.py
@@ -1,13 +1,20 @@
 import tkinter as tk
 from tkinter import ttk
 
+def main():
+    app = Application()
+    app.mainloop()
+
 class Application(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Test App")
 
-app = Application()
-app.mainloop()
+
+if __name__() == "__main__":
+    main()
+
+
 
 # root = tk.Tk()
 # root.title("Test App")
